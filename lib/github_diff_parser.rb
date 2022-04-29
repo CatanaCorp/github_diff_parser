d84bdb416d3a2e2f1cdd9832a6b568d28f4dc11b
Edouard CHIN <chin.edouard@gmail.com>
2022-04-29 23:38:44 +0200

    Add a new `GithubDiffParser.parse_patch`:
    - This allows to parse the output of a git format-patch or when using
      the `patch` media type on the GitHub API
      (https://github.com/rails/rails/commit/abc.patch)
    
      The advantage of parsing a patch is that it allows to get the commit
      SHA from a particular change as well as the timestamp of the
      commit. This is othewise not possible with a simple diff.

--- lib/github_diff_parser.rb
+++ lib/github_diff_parser.rb
@@ -3,13 +3,16 @@
 require_relative "github_diff_parser/version"
 
 module GithubDiffParser
-  InvalidDiff = Class.new(ArgumentError)
+  InvalidDiff  = Class.new(ArgumentError)
+  InvalidPatch = Class.new(ArgumentError)
 
-  autoload :Parser,  "github_diff_parser/parser"
-  autoload :Regexes, "github_diff_parser/regexes"
-  autoload :Diff,    "github_diff_parser/diff"
-  autoload :Hunk,    "github_diff_parser/hunk"
-  autoload :Line,    "github_diff_parser/line"
+  autoload :Diff,         "github_diff_parser/diff"
+  autoload :DiffParser,   "github_diff_parser/diff_parser"
+  autoload :Hunk,         "github_diff_parser/hunk"
+  autoload :Line,         "github_diff_parser/line"
+  autoload :Patch,        "github_diff_parser/patch"
+  autoload :PatchParser,  "github_diff_parser/patch_parser"
+  autoload :Regexes,      "github_diff_parser/regexes"
 
   extend self
 
@@ -20,7 +23,41 @@ module GithubDiffParser
   #   a correctly formatter Git Diff.
   #
   # @return [Array<GitubDiffParser::Diff>]
-  def parse(string)
-    Parser.new(string).process
+  def parse_diff(string)
+    DiffParser.new(string).process
+  end
+  alias_method :parse, :parse_diff
+
+  # Parse the output of a git format-patch. The difference between this method
+  # and the +parse_diff+ is that it allows to you to get information for each diff
+  # such as the commit sha and the date of the commit.
+  #
+  # @example A correctly formatted git format-patch output
+  #   From 21e02a7fd129a0c17e3dfbf39c6e69240c3dc3d2 Mon Sep 17 00:00:00 2001
+  #   From: Edouard CHIN <chin.edouard@gmail.com>
+  #   Date: Fri, 15 Apr 2022 12:22:33 +0200
+  #   Subject: [PATCH] Line added
+  #
+  #   ---
+  #    app/my_file.rb | 1 +
+  #    1 file changed, 1 insertion(+)
+  #
+  #   diff --git a/app/my_file.rb b/app/my_file.rb
+  #   index d3dfbe4..03d99f2 100644
+  #   --- a/app/my_file.rb
+  #   +++ b/app/my_file.rb
+  #   @@ -6,5 +6,6 @@ def test1
+  #
+  #      def test2
+  #        "This file is just added"
+  #   +    "This is a new line"
+  #      end
+  #    end
+  #
+  # @param string [String] the output of a git format-patch
+  #
+  # @return [Array<GitubDiffParser::Patch>]
+  def parse_patch(string)
+    PatchParser.new(string).process
   end
 end
--- lib/github_diff_parser/diff_parser.rb
+++ lib/github_diff_parser/diff_parser.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module GithubDiffParser
-  class Parser
+  class DiffParser
     # @param git_diff [String] the Git Diff output
     def initialize(git_diff)
       @git_diff = git_diff

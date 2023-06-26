16e6d6ac706af33f2ea0b63498f4e74d22b534a2
Edouard CHIN <chin.edouard@gmail.com>
2023-06-26 17:49:11 +0200

    Revert "Merge pull request #5 from Edouard-chin/ec-parse-patch"
    This reverts commit 2b59c4103bbb9d133f91f5a34f6138dc274a6d53, reversing
    changes made to 0f0b20156698d41b8fdc0a45a68dc233cbcbcacd.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,11 +10,7 @@ require "pathname"
 module Minitest
   class Test
     def read_diff(diff_file)
-      Pathname.new(__dir__).join("data", "diffs", "#{diff_file}.diff").read
-    end
-
-    def read_patch(patch_file)
-      Pathname.new(__dir__).join("data", "patches", "#{patch_file}.patch").read
+      Pathname.new(__dir__).join("data", "#{diff_file}.diff").read
     end
   end
 end
